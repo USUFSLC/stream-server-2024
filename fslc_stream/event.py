ef8ba0921e158ffff58a85ee0adf68ed2086fd2d
Richard Snider <lighthousemaniac@gmail.com>
2025-11-15 01:02:17 -0700

    add endpoint to get individual event as ics

--- fslc_stream/event.py
+++ fslc_stream/event.py
@@ -36,13 +36,14 @@ def new_event():
 @blueprint.get("")
 def get_events():
     with_streams = "with-streams" in request.args
-    query = select(Event) \
-        .order_by(Event.start_time)
 
     format = request.args.get("format", "json")
 
     if format not in ("json", "ics"):
         return make_response("invalid output format", 400)
+
+    query = select(Event) \
+        .order_by(Event.start_time)
 
     if "from" in request.args:
         from_time = request.args["from"]
@@ -87,12 +88,29 @@ def get_events():
 @blueprint.get("/<uuid:uuid>")
 def get_event(uuid: UUID):
     with_streams = "with-streams" in request.args
+
+    format = request.args.get("format", "json")
+
+    if format not in ("json", "ics"):
+        return make_response("invalid output format", 400)
+
     query = select(Event).where(Event.id == uuid)
     event = db.session.scalar(query)
 
     if event is None:
         return make_response("No such event.", 404)
-    return event.as_json(with_streams)
+
+    if format == "json":
+        return event.as_json(with_streams)
+    elif format == "ics":
+        result = icalendar.Calendar()
+        result.add_component(event.as_ics())
+
+        response = make_response(result.to_ical())
+        response.headers["content-type"] = "text/calendar"
+        return response
+    else:
+        return make_response("Somehow I didn't realize this was an illegal format before. Go back.", 400)
 
 
 @blueprint.delete("/<uuid:uuid>")
@@ -116,7 +134,7 @@ def patch_event(uuid: UUID):
         return make_response("need json object to update event", 400)
 
     query = select(Event).where(Event.id == uuid)
-    event: Event = db.session.scalar(query)
+    event: Event | None = db.session.scalar(query)
 
     if event is None:
         return make_response("no such event", 400)
